1aa01238122533c76ade4f9cf4eee902c2f17812
pro-utkarshM <projects.utkarshMaurya@gmail.com>
2024-06-01 01:46:09 +0530

    adding: script

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <commit-message>"
+  exit 1
+fi
+
+COMMIT_MESSAGE="$1"
+
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push
+if [ $? -eq 0 ]; then
+  echo "Changes pushed successfully!"
+else
+  echo "Failed to push changes."
+fi
